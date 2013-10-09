d330f10c1bb6613631972e779b891c15ec9a0bf6
Fanjing-LAB <devilkuro@gmail.com>
2013-10-09 19:38:43 +0800

    1310091938
    improve the performance of array list.

--- src/CArrayList.cpp
+++ src/CArrayList.cpp
@@ -32,11 +32,36 @@ CArrayList::~CArrayList() {
 }
 
 bool CArrayList::Insert(Request r) {
-	unsigned int st = time+r.ts;
-	unsigned int et = st+r.td;
 	if(accept(r)){
-		for(unsigned int i = st;i<et;i++){
-			resource[getIndex(i)] += r.bw;
+		unsigned int st = time+r.ts;
+		unsigned int et = st+r.td;
+		unsigned int tl = getIndex(time);
+		unsigned int sl = getIndex(st);
+		unsigned int el = getIndex(et);
+		if(sl>= tl){
+			// the start local is after the time local.
+			if(el>=tl){
+				// the start local and the end local are both after the time local.
+				// in this situation, just modify each time point one by one.
+				for(;sl<el;sl++){
+					resource[sl] += r.bw;
+				}
+			}else{
+				// the start local is after the time local, but the end local is before the time local.
+				// in this situation, modify each time point in two parts.
+				for (; sl < array_size; sl++) {
+					resource[sl] += r.bw;
+				}
+				for(sl = 0;sl<el; sl++){
+					resource[sl] += r.bw;
+				}
+			}
+		}else{
+			// the start local is before the time local.
+			// in this situation, just modify each time point from start local to end local one by one.
+			for(;sl<el;sl++){
+				resource[sl] += r.bw;
+			}
 		}
 		return true;
 	}else{
@@ -45,9 +70,27 @@ bool CArrayList::Insert(Request r) {
 }
 
 void CArrayList::setTime(unsigned int t) {
-	// the nodes before time t need to be reset.
-	for(;time<t;time++){
-		resource[getIndex(time)] = 0;
+	// the nodes before time t need to be reset. (t must not smaller than time)
+	if(t>=time+array_size){
+		// all points need to be reset.
+		memset(resource,0,array_size*sizeof(unsigned int));
+		time = t;
+	}else{
+		unsigned int tl = getIndex(time);
+		unsigned int el = getIndex(t);
+		unsigned int size_uint = sizeof(unsigned int);
+		if(el>tl){
+			// the end local is after the time local.
+			// in this situation, just reset the point from time local to end local.
+			memset(resource+tl,0,(el-tl)*size_uint);
+			time = t;
+		}else{
+			// the end local is before the time local.
+			// in this situation, the reset process should be divided into two parts.
+			memset(resource+tl,0,(array_size-tl)*size_uint);
+			memset(resource,0,el*size_uint);
+			time = t;
+		}
 	}
 }
 
@@ -57,9 +100,40 @@ bool CArrayList::accept(Request r) {
 	}else{
 		unsigned int st = time+r.ts;
 		unsigned int et = st+r.td;
-		for(unsigned int i = st;i<et;i++){
-			if(resource[getIndex(i)] + r.bw > max_resource){
-				return false;
+		unsigned int tl = getIndex(time);
+		unsigned int sl = getIndex(st);
+		unsigned int el = getIndex(et);
+		if(sl>= tl){
+			// the start local is after the time local.
+			if(el>=tl){
+				// the start local and the end local are both after the time local.
+				// in this situation, just compare each time point one by one.
+				for(;sl<el;sl++){
+					if(resource[sl] + r.bw > max_resource){
+						return false;
+					}
+				}
+			}else{
+				// the start local is after the time local, but the end local is before the time local.
+				// in this situation, compare each time point in two parts.
+				for (; sl < array_size; sl++) {
+					if (resource[sl] + r.bw > max_resource) {
+						return false;
+					}
+				}
+				for(sl = 0;sl<el; sl++){
+					if (resource[sl] + r.bw > max_resource) {
+						return false;
+					}
+				}
+			}
+		}else{
+			// the start local is before the time local.
+			// in this situation, just compare each time point from start local to end local one by one.
+			for(;sl<el;sl++){
+				if(resource[sl] + r.bw > max_resource){
+					return false;
+				}
 			}
 		}
 		return true;
