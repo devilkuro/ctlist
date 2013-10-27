d86698ee1088356b0efd05b6cd9800fcb32f1285
Fanjing <devilkuro@gmail.com>
2013-10-27 14:02:59 +0800

    1310271402: fix a bug when change the tnum.

--- src/CTackedLinkList.cpp
+++ src/CTackedLinkList.cpp
@@ -19,10 +19,11 @@ template <class T> string m_toStr(T tmp)
     return ss.str();
 }
 int main(){
-	// experiment 1
 	Helper H;
-	// 1.1st generate the request set.
 	Request* rq = new Request[REQUEST_NUM];
+/*
+	// experiment 1
+	// 1.1st generate the request set.
 
 	srand(0);
 	for(unsigned int i = 0;i<REQUEST_NUM;i++){
@@ -52,12 +53,14 @@ int main(){
 		file << endl;
 		file.close();
 	}
+*/
 
 	ofstream file2("result2.txt");
 	file2 << "td/tnum\t8\t16\t32\t64" <<endl;
 	file2.close();
 	// experiment 2
 	for(unsigned int i = 2;i<=200;i+=2){
+		srand(0);
 		for(unsigned int j = 0;j<REQUEST_NUM;j++){
 			rq[i].ts = H.U_Randint(1,2000-i);
 			rq[i].td = i;
@@ -67,7 +70,7 @@ int main(){
 		file << i ;
 		cout << i << endl;
 		for(unsigned int j = 8;j<=64;j*=2){
-			CTLink* ct = new CTLink(i,2000,2000);
+			CTLink* ct = new CTLink(j,2000,2000);
 			unsigned int t = 0;
 			clock_t start = clock();
 			for(unsigned int k = 0;k<REQUEST_NUM;k++){
@@ -84,7 +87,7 @@ int main(){
 	// experiment 3
 	// TODO:add experiment 3~5.
 
-	// experiment 6
+/*	// experiment 6
 	srand(0);
 	unsigned int *interval = new unsigned int[REQUEST_NUM];
 	for (unsigned int i = 0; i < REQUEST_NUM; i++) {
@@ -144,7 +147,7 @@ int main(){
 		}
 		file << endl;
 		file.close();
-	}
+	}*/
 }
 
 
