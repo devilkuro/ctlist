103ed9840715e1062e9570c1e3c412da2bf70f1b
Fanjing-LAB <devilkuro@gmail.com>
2016-10-02 15:17:44 +0800

    update ASMTimer

--- src/utils/ASMTimer.cc
+++ src/utils/ASMTimer.cc
@@ -8,46 +8,6 @@
 #include "iostream"
 
 ASMTimer* ASMTimer::ptr_singleton = NULL;
-ASMTimer::ASMTimer() {
-    unsigned long startHigh = 0;
-    unsigned long startLow = 0;
-    unsigned long endHigh = 0;
-    unsigned long endLow = 0;
-
-    __asm__ __volatile__(
-            ".intel_syntax noprefix\n\t"
-            "RDTSC\n\t"
-            "mov        %0,     %%edx\n\t"
-            "mov        %1,     %%eax\n\t"
-            ".att_syntax\n"
-            :"=r"(startHigh),"=r"(startLow)
-            :
-            : "%eax","%edx"
-    );
-    Sleep(1000);
-    __asm__ __volatile__(
-            ".intel_syntax noprefix\n\t"
-            "RDTSC\n\t"
-            "mov        %0,     %%edx\n\t"
-            "mov        %1,     %%eax\n\t"
-            ".att_syntax\n"
-            :"=r"(endHigh),"=r"(endLow)
-            :
-            : "%eax","%edx"
-    );
-    counter_start.HighPart = startHigh;
-    counter_start.LowPart = startLow;
-    counter_end.HighPart = endHigh;
-    counter_end.LowPart = endLow;
-    frequency.QuadPart = counter_end.QuadPart - counter_start.QuadPart;
-    initialized = frequency.QuadPart > 0 ? true : false;
-
-    if(!initialized){
-        return;
-    }
-    isTiming = false;
-    gotTime = false;
-}
 
 const bool ASMTimer::available() const {
     return initialized;
@@ -57,23 +17,23 @@ ASMTimer::~ASMTimer() {
 }
 
 ASMTimer* ASMTimer::request() {
-    if(ptr_singleton == NULL){
+    if (ptr_singleton == NULL) {
         ptr_singleton = new ASMTimer();
     }
     return ptr_singleton;
 }
 
 void ASMTimer::release() {
-    if(ptr_singleton != NULL){
+    if (ptr_singleton != NULL) {
         delete ptr_singleton;
         ptr_singleton = NULL;
     }
 }
 
 const UINT64 ASMTimer::getCounts() const {
-    if(gotTime){
+    if (gotTime) {
         return (counter_end.QuadPart - counter_start.QuadPart);
-    }else{
+    } else {
         return 0;
     }
 }
@@ -83,10 +43,18 @@ const UINT64 ASMTimer::getFrequency() const {
 }
 
 const UINT64 ASMTimer::getMilliseconds() const {
-    if(gotTime){
+    if (gotTime) {
         return ((counter_end.QuadPart - counter_start.QuadPart)
                 / (frequency.QuadPart / 1000));
-    }else{
+    } else {
+        return 0;
+    }
+}
+
+const INT64 ASMTimer::getECC() const {
+    if (initialized) {
+        return ecc;
+    } else {
         return 0;
     }
 }
--- src/utils/ASMTimer.h
+++ src/utils/ASMTimer.h
@@ -9,10 +9,12 @@
 #define _ASMTIMER_H_
 
 #include <windows.h>
+#include <iostream>
 
 class ASMTimer {
 private:
     // singleton mode
+    int initRound;
     static ASMTimer* ptr_singleton;
     ASMTimer();  // default constructor, perform initializations
     virtual ~ASMTimer();
@@ -23,6 +25,7 @@ protected:
     bool gotTime;     // if the timer has stopped and successfully got the time
 
     ULARGE_INTEGER frequency; // the current performance-counter frequency, in counts per second
+    INT64 ecc;
     ULARGE_INTEGER counter_start; // value of the performance counter at the moment when start() is called
     ULARGE_INTEGER counter_end; // value of the performance counter at the moment when end() is called
 public:
@@ -36,39 +39,72 @@ public:
     const UINT64 getCounts() const; //get the time interval in counts
     const UINT64 getMilliseconds() const; // get the time interval length in milliseconds
     const UINT64 getFrequency() const;
+    const INT64 getECC() const;
 };
 
+inline ASMTimer::ASMTimer() {
+    unsigned long startHigh = 0;
+    unsigned long startLow = 0;
+    unsigned long endHigh = 0;
+    unsigned long endLow = 0;
+    initialized = true;
+
+    isTiming = false;
+    gotTime = false;
+    initRound = 10;
+    ULARGE_INTEGER t = 0; // 用于消除误差
+    for (int i = 0; i < initRound; ++i) {
+        start();
+        end();
+        t = getCounts();
+        ecc = t < ecc ? t : ecc;
+    }
+    start();
+    Sleep(1000);
+    end();
+    counter_start.HighPart = startHigh;
+    counter_start.LowPart = startLow;
+    counter_end.HighPart = endHigh;
+    counter_end.LowPart = endLow;
+    frequency.QuadPart = counter_end.QuadPart - counter_start.QuadPart;
+    initialized = frequency.QuadPart > 0 ? true : false;
+    gotTime = false;
+    if (!initialized) {
+        std::cout << "initialize asm timer failed" << std::endl;
+    }
+}
+
 inline void ASMTimer::start() {
-    if(initialized && !isTiming){
+    if (initialized && !isTiming) {
         isTiming = true;
         gotTime = false;
         unsigned long startHigh = 0;
         unsigned long startLow = 0;
+
         __asm__ __volatile__(
-                ".intel_syntax noprefix\n\t"
-                "RDTSC\n\t"
-                "MOV        %0,     %%edx\n\t"
-                "MOV        %1,      %%eax\n\t"
-                ".att_syntax\n"
+                "RDTSC              ;"
+                "mov    %0, %%edx   ;"
+                "mov    %1, %%eax   ;"
                 :"=r"(startHigh),"=r"(startLow)
                 :
                 : "%eax","%edx"
         );
         counter_start.HighPart = startHigh;
         counter_start.LowPart = startLow;
+    } else {
+        std::cout << "start asm timer failed" << std::endl;
     }
 }
 
 inline void ASMTimer::end() {
-    if(isTiming){
+    if (isTiming) {
         unsigned long endHigh = 0;
         unsigned long endLow = 0;
+
         __asm__ __volatile__(
-                ".intel_syntax noprefix\n\t"
-                "RDTSC\n\t"
-                "MOV        %0,     %%edx\n\t"
-                "MOV        %1,     %%eax\n\t"
-                ".att_syntax\n"
+                "RDTSC              ;"
+                "mov    %0, %%edx   ;"
+                "mov    %1, %%eax   ;"
                 :"=r"(endHigh),"=r"(endLow)
                 :
                 : "%eax","%edx"
@@ -77,6 +113,11 @@ inline void ASMTimer::end() {
         counter_end.LowPart = endLow;
         isTiming = false;
         gotTime = true;
+    } else {
+        std::cout << "end asm timer failed" << std::endl;
     }
 }
+
+
+
 #endif /* _ASMTIMER_H_ */
